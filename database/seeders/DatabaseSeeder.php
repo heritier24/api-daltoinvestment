aff3b03cef643bee57a4dbd24e66219633c33aa3
Heritier Ganza Tamba <heritierganzatamba@gmail.com>
2025-03-26 21:09:00 +0200

    company wallet and deposit initiated well

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\CompanyWallet;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Seed company wallets for each network
+        $wallets = [
+            ['network' => 'BSC', 'address' => 'BSC9876zyxw5432vuts1098'],
+            ['network' => 'TRC20', 'address' => 'TRC20klmn5678opqr9012abcd'],
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($wallets as $wallet) {
+            CompanyWallet::create($wallet);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\DepositController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+    Route::get('/company-wallets', [DepositController::class, 'getCompanyWallet']);
+    Route::post('/deposits', [DepositController::class, 'createDeposit']);
 });
 
 Route::post('/register', [AuthenticationController::class, 'register']);
